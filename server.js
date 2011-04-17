b927ce1ca4771c51572ba0e9577507260a68bee0
Jacob Thornton <jacobthornton@gmail.com>
2011-04-17 15:16:20 -0700

    adds realtime building support powered by ender! hhooo shit!!...

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express')
   , fs = require('fs')
+  , path = require('path')
   , stache = require('stache')
   , md = require('markdown-js')
+  , ender = require('ender')
   , app = express.createServer();
 
 //config
@@ -9,6 +11,7 @@ app.set('view engine', 'mustache')
 app.set("views", __dirname + '/views');
 app.register(".mustache", stache);
 app.use(express.static(__dirname + '/public'));
+app.use(express.bodyParser());
 
 //routes
 app.get('/', function (req, res) {
@@ -21,6 +24,27 @@ app.get('/', function (req, res) {
       },
     });
   });
+});
+
+app.get('/download', function (req, res) {
+  var name = './tmp/ender.' + (+new Date)
+    , filepath = name + '.min.js';
+
+  ender.exec(req.param('cmd'), name, function () {
+    res.download(path.join(__dirname, filepath), 'ender.min.js', function (err) {
+      if (err) throw err;
+      var complete = 0
+        , isComplete = function (err) {
+            if (err) throw err;
+            if (++complete == 2) {
+              console.log('successfully deleted tmp files');
+            }
+        };
+      fs.unlink(name + '.js', isComplete);
+      fs.unlink(filepath, isComplete);
+    });
+  });
+
 });
 
 //Run
