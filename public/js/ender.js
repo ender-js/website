a3d97e4b7da5cae616b6409025f430b3997e88d2
Rod Vagg <rod@vagg.org>
2013-04-08 12:15:06 +1000

    reunify app.js into ender.js build

--- public/js/ender.js
+++ public/js/ender.js
@@ -1,8 +1,8 @@
 /*!
   * =============================================================
   * Ender: open module JavaScript framework (https://ender.no.de)
-  * Build: ender build domready qwery bonzo bean jeesh morpheus
-  * Packages: ender-js@0.4.4-1 domready@0.2.11 qwery@3.4.1 bonzo@1.3.4 bean@1.0.3 jeesh@0.0.6 morpheus@0.6.6
+  * Build: ender build
+  * Packages: ender-js@0.4.4-1 domready@0.2.11 qwery@3.4.1 bonzo@1.3.4 bean@1.0.3 jeesh@0.0.6 morpheus@0.6.6 ender-website-frontend@0.0.0
   * =============================================================
   */
 
@@ -3195,4 +3195,177 @@
       tween: morpheus.tween
     })
   }(ender)
+}());
+
+(function () {
+
+  var module = { exports: {} }, exports = module.exports;
+
+  /*global $, WebFontConfig*/
+
+  /**
+    * @FAT + @DED + @RVAGG
+    * APP SINGLETON CONTR.
+    * LICENSE MIT
+    * ====================
+    */
+
+  var App = {
+
+    /* LE APP VARS
+      ====================*/
+
+      $nav: null
+    , $win: null
+    , $buttons: null
+
+    , navTop: null
+
+    , fixed: null
+    , activeSection: null
+
+    , sections: null
+    , sectionCoords: null
+
+    /* LE APP METHODS
+      ====================*/
+
+    , init: function () {
+
+        this.$nav        = $('#nav')
+        this.$buttons    = $('#nav a:not(.alt)')
+        this.$win        = $(window)
+        this.$page       = $('#content .page')
+        this.pages       = [ 'main', 'news' ]
+        this.currentPage = 0
+
+        this.measure()
+
+        this.$win.bind('scroll', this.processScroll)
+        this.$nav.delegate(this.$buttons, 'click', this.setButton)
+
+        $('#nav a').on('click', function (e) {
+          var href = $(e.currentTarget).attr('href')
+            , ho
+
+          if (!/^#/.test(href))
+            return
+
+          e.stop()
+
+          href = href.substring(1)
+          ho = $('#' + href).offset()
+
+          App.scrollTo(ho.top + ho.height - 20)
+          App.navTo(href)
+        })
+
+        var page = location.href.split('#')[1]
+        if (page)
+          App.navTo(page)
+      }
+
+    , measure: function () {
+        var navPos = App.$nav.css('position')
+          , navTop = App.$nav.css('top')
+          , navOffset
+          , section
+          , sto
+          , i = 0
+          , l
+
+        App.$nav.css({
+            position: 'relative'
+          , top: ''
+        })
+        navOffset         = App.$nav.offset()
+        App.navTop        = navOffset.top
+        App.sections      = []
+        App.sectionCoords = []
+
+        App.$nav.css('position', 'absolute')
+
+        for (l = App.$buttons.length; i < l; i++) {
+          App.sections.push($(App.$buttons[i]).html())
+          if (section = document.getElementById(App.sections[i])) {
+            sto = $(section).offset()
+            App.sectionCoords.push(sto.top + sto.height - 25)
+          }
+        }
+
+        App.$nav.css({
+            position : navPos
+          , top      : navTop
+        })
+        App.processScroll()
+      }
+
+    , navTo: function (page_) {
+        var page = page_.replace(/__.*$/, '')
+          , io = this.pages.indexOf(page)
+        location.href = location.href.replace(/(#.*$)|($)/, '#' + (page_ || ''))
+        console.log('navTo(', page_, '):', page, io)
+        if (io == -1)
+          io = 0
+        if (this.currentPage != io) {
+          this.$page.removeClass(this.pages[this.currentPage])
+          this.$page.addClass(this.pages[this.currentPage = io])
+          if (io !== 0) {
+            App.activeSection = null
+            this.setButton.call($('nav a[href=#' + page + ']')[0])
+          } else {
+            if (page_ != page)
+              this.processScroll()
+          }
+        }
+      }
+
+    , setButton: function () {
+        $('nav a').attr('class', '')
+        $(this).addClass('yellow')
+      }
+
+    , processScroll: function () {
+        var scrollTop = App.$win.scrollTop()
+          , i         = App.sectionCoords.length
+          , isActive
+
+        if (App.currentPage === 0) {
+          for (; i--;) {
+            isActive = App.activeSection != App.sections[i]
+                && scrollTop > App.sectionCoords[i]
+                && (!App.sectionCoords[i + 1] || scrollTop < App.sectionCoords[i + 1])
+
+            if (isActive) {
+              App.activeSection = App.sections[i]
+              App.setButton.call($('nav a[href=#' + App.sections[i] + ']')[0])
+            }
+          }
+        }
+
+        if (scrollTop >= App.navTop && !this.fixed) {
+          App.fixed = true
+          App.$nav.css({
+              position: 'fixed'
+            , top: 0
+          })
+        } else if (scrollTop <= App.navTop && App.fixed) {
+          App.fixed = false
+          App.$nav.attr('style', '')
+        }
+      }
+
+    , scrollTo: function (pos, callback) {
+        var fn = function (t) { window.scrollTo(0, Math.round(t)) }
+        $.tween(400, fn, callback, null, $(window).scrollTop(), pos)
+      }
+  }
+
+  $.domReady(function () {
+    App.init()
+  })
+
+  WebFontConfig._loaded = App.measure
+  if (typeof provide == "function") provide("ender-website-frontend", module.exports);
+  $.ender(module.exports);
 }());
