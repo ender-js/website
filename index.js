37593761e1ae04196bc0a13f54b2fa711c56a94b
Rod Vagg <rod@vagg.org>
2013-07-26 21:48:39 +1000

    upgrade to splinky 0.1.0

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-var path        = require('path')
-  , splinksmvc  = require('splink-smvc')
-  , isDev       = (/^dev/i).test(process.env.NODE_ENV)
+const path    = require('path')
+    , Splinky = require('splinky')
+    , isDev   = (/^dev/i).test(process.env.NODE_ENV)
 
 // init swig with 'root' param, this isn't done by `consolidate` but required by
 // swig if you want to reference templates from within templates
 require('swig').init({ root: path.join(__dirname, 'views'), cache: !isDev })
 
-splinksmvc({
-    port     : process.env.PORT || 3000
-  , scan     : [
-        path.join(__dirname, './lib')
-    ]
-  , 'static' : {
-        path       : path.join(__dirname, './public')
-      , url        : '/'
-      , cache      : isDev ? false : {}
-    }
-  , 'views'  : {
-        path       : path.join(__dirname, './views')
-      , suffix     : 'html'
-      , processor  : 'swig'
-    }
-}).start()
+var splinky = Splinky({ port: process.env.PORT || 3000 })
+splinky.scan(path.join(__dirname, './lib'))
+splinky.static({
+    path       : path.join(__dirname, './public')
+  , url        : '/'
+  , cache      : isDev ? false : {}
+})
+splinky.views({
+    path       : path.join(__dirname, './views')
+  , suffix     : 'html'
+  , processor  : 'swig'
+})
+
+splinky.listen()
